24b001c6e7244446d51a37a44cb90e00a0703497
Bjoern Schiessle <schiessle@owncloud.com>
2015-08-18 18:19:30 +0200

    occ command to encrypt all files

---
+++ apps/encryption/templates/altmail.php
@@ -0,0 +1,13 @@
+<?php
+print_unescaped($l->t("Hey there,\n\nthe admin enabled server-side-encryption. Your files was encrypted using the password '%s'.\n\nPlease login to the web interface, go to the section 'ownCloud basic encryption module' of your personal settings and update your encryption password by entering this password and your current login-password.\n\n", array($_['password'])));
+if ( isset($_['expiration']) ) {
+	print_unescaped($l->t("The share will expire on %s.", array($_['expiration'])));
+	print_unescaped("\n\n");
+}
+// TRANSLATORS term at the end of a mail
+p($l->t("Cheers!"));
+?>
+
+	--
+<?php p($theme->getName() . ' - ' . $theme->getSlogan()); ?>
+<?php print_unescaped("\n".$theme->getBaseUrl());
